9c02039bdd5f99f3758c74f877f88cb6e331f478
Adam Cooper <arc@hilltop-cottage.info>
2023-10-31 16:29:16 +0000

    streamline specification id check

--- AttributeIssuesFlask/__init__.py
+++ AttributeIssuesFlask/__init__.py
@@ -1,4 +1,4 @@
-from os import  environ
+# from os import  environ
 import logging
 
 from flask import Flask, render_template, session, request, abort, Blueprint
@@ -35,19 +35,14 @@ def validate():
 def questionnaire(specification_id: str):
     view_name = "questionnaire"
 
-    if specification_id not in core.specification_ids:
-        msg = f"Request with invalid specification id = {specification_id} for plaything {PLAYTHING_NAME}"
-        logging.warn(msg)
-        abort(404, msg)
-
-    core.record_activity(view_name, specification_id, session, referrer=request.referrer, tag=request.args.get("tag", None))
-    
     spec = core.get_specification(specification_id)
     langstrings = Langstrings(spec.lang)
-    post_url = f"{plaything_root}/processed/{specification_id}?" + request.query_string.decode()
+
+    core.record_activity(view_name, specification_id, session, referrer=request.referrer, tag=request.args.get("tag", None))
+
     return render_template("questionnaire.html",
                            langstrings=langstrings,
-                           post_url=post_url,
+                           post_url=f"{plaything_root}/processed/{specification_id}?" + request.query_string.decode(),
                            title=spec.title,
                            attribute_scope=spec.detail.get("attribute_scope", None),
                            key_question=spec.detail.get("key_question", None),
@@ -59,15 +54,10 @@ def questionnaire(specification_id: str):
 def processed(specification_id: str):
     view_name = "processed"
 
-    if specification_id not in core.specification_ids:
-        msg = f"Request with invalid specification id = {specification_id} for plaything {PLAYTHING_NAME}"
-        logging.warn(msg)
-        abort(404, msg)
-
-    selected_attributes = list(request.form)  # gets a list of the checkbox name attributes for those which are "checked". We must determine unchecked by difference.
-
     spec = core.get_specification(specification_id)
     langstrings = Langstrings(spec.lang)
+
+    selected_attributes = list(request.form)  # gets a list of the checkbox name attributes for those which are "checked". We must determine unchecked by difference.
 
     attributes = spec.load_asset_records_dict("attributes")
     for attribute in attributes:
@@ -104,17 +94,17 @@ def processed(specification_id: str):
 def about(specification_id: str):
     view_name = "about"
 
-    core.record_activity(view_name, specification_id, session, referrer=request.referrer, tag=request.args.get("tag", None))
     spec = core.get_specification(specification_id)
+    langstrings = Langstrings(spec.lang)
+
     if "about" not in spec.asset_map:
         abort(404, "'about' is not configured")
 
-    langstrings = Langstrings(spec.lang)
+    core.record_activity(view_name, specification_id, session, referrer=request.referrer, tag=request.args.get("tag", None))
 
     return render_template("about.html",
                            about=spec.load_asset_markdown(view_name, render=True),
                            top_menu=spec.make_menu(menu, langstrings, plaything_root, view_name, query_string=request.query_string.decode()))
-
 
 app = prepare_app(Flask(__name__), url_prefix=plaything_root)
 app.register_blueprint(pt_bp, url_prefix=plaything_root)
